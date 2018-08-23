b588f0b85e87ca0d4014f0110e6cf8fb1134bf55
우아테크캠프_32 <>
2018-08-23 16:17:27 +0900

    test 코드 수정

--- bazzangee/src/test/java/com/woowahan/smell/bazzangee/web/api/ApiUserAcceptanceTest.java
+++ bazzangee/src/test/java/com/woowahan/smell/bazzangee/web/api/ApiUserAcceptanceTest.java
@@ -1,28 +1,24 @@
 package com.woowahan.smell.bazzangee.web.api;
 
-import com.woowahan.smell.bazzangee.domain.User;
-import com.woowahan.smell.bazzangee.dto.UserJoinDto;
 import com.woowahan.smell.bazzangee.dto.UserLoginDto;
-import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import support.test.AcceptanceTest;
 
-import java.time.LocalDate;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class ApiUserAcceptanceTest extends AcceptanceTest {
     private UserLoginDto userLoginDto;
+
     @Before
     public void setUp() throws Exception {
         userLoginDto =
                 UserLoginDto.builder()
-                .userId("serverwizard@naver.com")
-                .password("password").build();
+                        .userId("serverwizard@naver.com")
+                        .password("password").build();
     }
 
     @Test
